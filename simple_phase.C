cbbb607a9e632e741fad7d8600fa8cbd42deb95d
jon <jonmott46@gmail.com>
2025-10-06 14:03:57 -0400

    phase_fit now outputs a vector of fit params. Will update output as needed. Angular frequency is now a fixed parameter in the fits. Temporarily set to 2*pi*240 Hz (likely inputted in wrong format, verify this)

--- simple_phase.C
+++ simple_phase.C
@@ -11,14 +11,14 @@ using namespace std;
 //
 ///////////////////////////////////////////////////////////////
 
-void phase_fit(int run_num, int event_min, int event_max, string type, string name, Double_t *params);
+vector<double> phase_fit(int run_num, int event_min, int event_max, string type, string name, string cut);
 
 
 //This function takes the following info and performs a sinusoidal fit: Run #, the event range (min/max), the type of object (currently magnet/bpm), and the specific name (of said magnet/bpm)
-void simple_phase(int run_num, int event_min, int event_max, string type, string name) {
+void simple_phase(int run_num, int event_min, int event_max, string type, string name, string cut) {
 
-  Double_t params[4]; //Fit parameters. Stores fit data after the fit has been performed. See definitions below
-  phase_fit(run_num, event_min, event_max, type, name, params);
+  //Double_t params[4]; //Fit parameters. Stores fit data after the fit has been performed. See definitions below
+  vector<double> params = phase_fit(run_num, event_min, event_max, type, name, cut);
 
   cout << "\nYour fit parameters are as follows\n" << endl;
   cout << "Amplitude: " << params[0] << endl;
@@ -38,9 +38,9 @@ void simple_phase(){
   int event_max = 7100;
   string type = "bpm";
   string name = "bpm11Y";
+  string cut = "ch_cleandata==1 && ch_scandata1==0 && bcm_dg_ds>4";
 
-  Double_t params[4];
-  phase_fit(run_num, event_min, event_max, type, name, params);
+  vector<double> params = phase_fit(run_num, event_min, event_max, type, name, cut);
 
   cout << "Your fit parameters are as follows" << endl;
   cout << "Amplitude: " << params[0] << endl;
@@ -53,22 +53,20 @@ void simple_phase(){
 
 
 
-void phase_fit(int run_num, int event_min, int event_max,  string type, string name, Double_t *params) {
+vector<double> phase_fit(int run_num, int event_min, int event_max,  string type, string name, string cut) {
 
   TFile *f = new TFile(Form("/chafs2/work1/parity/japanOutput/sbs_CntHouse_%d.root",run_num),"READ"); //Loading the root file
-
   if (!f || f->IsZombie()) {
     cout << "Error opening file !" << endl;
-    return 1;
   }
+
   TTree *t = (TTree*)f->Get("evt"); //Loading the "evt" TTree
   if (!t) {
     std::cout << "Error: TTree not found!" << std::endl;
-    return;
   }
 
   //Draws the desired data from the TTree and moves it to a histogram for fitting
-  t->Draw(Form("%s:Entry$>>h",name.c_str()),Form("ch_cleandata==1 && ch_scandata1==0 && bcm_dg_ds>4 && Entry$>%d && Entry$<%d",event_min,event_max),"goff");
+  t->Draw(Form("%s:Entry$>>h",name.c_str()),Form("%s && Entry$>%d && Entry$<%d",cut.c_str(),event_min,event_max),"goff");
   TH2F* h = (TH2F*)gDirectory->Get("h");
 
   TF1 *fit = new TF1("fit", "[0]*sin([1]*x + [2]) + [3]", event_min, event_max); //Creating the sinusoidal fit
@@ -77,34 +75,33 @@ void phase_fit(int run_num, int event_min, int event_max,  string type, string n
   if (type == "magnet") {
     if (name == "sl20_rf_vernier") {
       fit->SetParameter(0, 3);      // Amplitude guess
-      fit->SetParameter(1, 1.55);   // Angular frequency guess (2π*f)
+      fit->FixParameter(1, 1.55);   // Angular frequency guess (2π*f)
       fit->SetParameter(2, 0.0);    // Phase guess
       fit->SetParameter(3, -256);   // Offset guess
     }
     else {
       fit->SetParameter(0, 10000);  // Amplitude guess
-      fit->SetParameter(1, 0.399);  // Angular frequency guess (2π*f)
+      fit->FixParameter(1, 1508.0);  // Angular frequency guess (2π*f). Used to be 0.399
       fit->SetParameter(2, 0.0);    // Phase guess
       fit->SetParameter(3, 150);    // Offset guess
     }
   }
   else if (type == "bpm") {
     fit->SetParameter(0, 0.1);      // Amplitude guess
-    fit->FixParameter(1, 0.399);    // Angular frequency guess (2π*f)
+    fit->FixParameter(1, 1508.0);   // Angular frequency guess (2π*f). Used to be 0.399
     fit->SetParameter(2, 0.0);      // Phase guess
     fit->SetParameter(3, 0.15);     // Offset guess
   }
   else {
     std::cout << "Error: type not valid" << std::endl;
-    return;
   }
-
   h->Fit("fit", "QN0"); //Applying the fit
-  //h->SaveAs("name.extension")
-
+  //h->SaveAs("name.png");
   //Filling params with the fit parameters for use outside this function
+  vector<double> params;
   for (int i = 0; i < 4; i++) {
-    params[i] = fit->GetParameter(i);
+    params.push_back(fit->GetParameter(i));
   }
+  return params;
 
 }
